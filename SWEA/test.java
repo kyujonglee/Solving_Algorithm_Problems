a5232b2e8af652e9a03d94d8f137b8c11f028f0a
kyujonglee <kyujong93@naver.com>
2019-01-10 22:56:50 +0900

    test 변경

--- SWEA/test.java
+++ SWEA/test.java
@@ -1,8 +1,53 @@
+import java.util.Arrays;
+//import java.lang.String;
 
 public class test {	
 	public static void main(String[] args) {
-		int a = '4'-'0'; 
-		System.out.println(a);
-//		System.out.println((int)(a-'0'));
+		char z = 'z';
+		System.out.println(z);
+		char a = 'a';
+		System.out.println(a+'0');
+		int b = 'a';
+		System.out.println(b);
+		int c = '0';
+		System.out.println(c);
+		char ch = 'A';
+		char ss = 'B';
+		System.out.println(ss-ch);
+		char ii = 65;
+		System.out.println(ii-'0');
+		String x = "what";
+		String y = "the";
+		System.out.println(x+y);
+		int[] arr = new int[3];
+		System.out.println(Arrays.toString(arr));
 	}
 }
+
+
+//
+//
+//String a="Heloo";
+//a.indexOf("java")
+//class String {
+//	int indexOf(String a) {
+//		
+//	}
+//	String toUpperCase() {
+//		
+//	}
+//	 String[] weeks = {"월", "화", "수", "목", "금", "토", "일"};
+//	String[] weeks = new String[7];
+//	weeks = new String[]{"월", "화", "수", "목", "금", "토", "일"};
+//	weeks = {"월", "화", "수", "목", "금", "토", "일"};
+//	int[] arr = new int[3];
+//	arr[arr.length-1];
+//	for(int i=0;i<arr.length;i++) {
+//		System.out.println(arr[i]);
+//		arr[i] = 1;
+//	}
+//	for(int num:arr) {
+//		System.out.println(num);
+//		num = 1;
+//	}
+//}
